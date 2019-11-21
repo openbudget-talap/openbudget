c2baa97a9f682d34bf9be4dc5558b3e33c1a3019
openbudget-talap <openbudget@talap.org>
2019-11-21 12:59:23 +0600

    Openbudget first load

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Talap_user\PycharmProjects\openbudget\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
